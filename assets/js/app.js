73e43f3ff54b88184411f0c709bb3f81563dd982
mutan <akim_now@mail.ru>
2019-06-21 01:41:53 +0300

    Working

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,8 @@ require('../css/app.scss');
 /* JS LIBRARIES */
 
 const $ = require('jquery');
+global.$ = global.jQuery = $;
 require('popper.js/dist/popper');
 require('bootstrap/dist/js/bootstrap');
+
+require('./angle');
