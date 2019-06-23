443d1f8b3de970acfbd6b2ef08597ec0e9f6f51d
mutan <akim_now@mail.ru>
2019-06-23 03:26:30 +0300

    Working

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,8 @@ require('datatables.net-bs4/js/dataTables.bootstrap4');
 
 /* CUSTOM JS */
 
+/* Datatables */
+
 $(document).ready( function () {
     $('.datatable').DataTable({
         "pagingType": "full_numbers",
@@ -41,3 +43,73 @@ $(document).ready( function () {
         }
     });
 } );
+
+/* Modal */
+
+let Modal = {
+    getModal: function() {
+        return $('#modalMain');
+    },
+
+    toggleButtonSpinnerIcon: function(e) {
+        $(e.currentTarget).find('i').toggleClass('fa-spinner fa-spin');
+    },
+
+    /* JQuery автодополнение */
+    /* http://api.jqueryui.com/autocomplete/ */
+    /*shopAutocomplete: function(id) {
+        $(id).autocomplete({
+            minLength: 2,
+            source: '/basket/shop/autocomplete',
+            select: function(event, ui) {
+                $(id).val(ui.item.value);
+                //$('#basket-shop-form').submit();
+            }
+        });
+    },*/
+
+    handleMainModal: function(e, options) {
+        e.preventDefault();
+        $.ajax({
+            url: options.url,
+            type: 'POST',
+            beforeSend: ()=> {Modal.toggleButtonSpinnerIcon(e);},
+            complete: ()=> {Modal.toggleButtonSpinnerIcon(e);}
+        }).then(function (response) {
+            Modal.reload(Modal.getModal(), response, options);
+        });
+    },
+
+    reload: function($modal, response, options) {
+        if (options.size) {
+            $modal.find('.modal-dialog').addClass(options.size);
+        }
+        $modal.find('.modal-content').html(response.output);
+        $modal.modal('show');
+        if (options.shopAutocomplete) {
+            Modal.shopAutocomplete(options.shopAutocompleteElem);
+        }
+
+        $modal.find('form').on('submit', (e)=> {
+            e.preventDefault();
+            let formData = $(e.currentTarget).serialize();
+            const $submitButton = $(e.currentTarget).find('button[type=submit]');
+            $.ajax({
+                url: options.url,
+                type: 'POST',
+                data: formData,
+                beforeSend: ()=> {
+                    $submitButton.prop('disabled', true).html("<i class='fa fa-spinner fa-spin pr-1'></i> Идет сохранение");
+                }
+            }).then(function (response) {
+                (response.reload) ? location.reload() : Modal.reload($modal, response, options);
+            });
+        });
+    }
+};
+
+$('#artist-new').on('click', (e)=> {
+    Modal.handleMainModal(e, {
+        url: `admin/artist/new`
+    });
+});
